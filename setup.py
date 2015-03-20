b223f2b8af985ccea88f73423a1b806cb1816a2b
Michael Persson <michael.ake.persson@gmail.com>
2015-03-20 13:05:57 +0100

    Added test for empty options

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ CLASSIFIERS = [
 
 setup(
     name             = 'etcd-cli',
-    version          = '0.1-7',
+    version          = '0.1-9',
 
     description      = 'etcd-cli CLI for Etcd based on schemas',
     long_description = open("README.rst").read(),
