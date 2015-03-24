225505b8370d1616c32836614ed2a0db7881f1f1
Michael Persson <michael.ake.persson@gmail.com>
2015-03-24 11:30:43 +0100

    Changed library to python-etcd which is maintained and supports recursive queries

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ CLASSIFIERS = [
 
 setup(
     name             = 'etcd-cli',
-    version          = '0.1-9',
+    version          = '0.2',
 
     description      = 'etcd-cli CLI for Etcd based on schemas',
     long_description = open("README.rst").read(),
